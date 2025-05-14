8a33f1d3ef995ff95b802df5c14e65493cc57aae
yuhen <yuhen@yH.eduroam.dhcp.monash.edu.my>
2025-05-14 10:40:08 +0800

    Processing v1

---
+++ Processing_STM/Core/Src/main.c
@@ -0,0 +1,682 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+TIM_HandleTypeDef htim1;
+TIM_HandleTypeDef htim6;
+TIM_HandleTypeDef htim16;
+
+UART_HandleTypeDef huart1;
+UART_HandleTypeDef huart2;
+
+/* USER CODE BEGIN PV */
+#include <stdio.h>
+#include <string.h>
+
+volatile uint8_t recording_mode = 0; // 0 = idle, 1 = manual, 2 = distance-triggered
+volatile uint8_t recording_active = 0; // Used to control distance mode logic
+
+// Audio reading variables
+uint8_t RX_Buffer[2] = {0};
+uint8_t MSB_8 = 0;
+uint8_t LSB_8 = 0;
+uint16_t ADC_output = 0;
+char string_1[40] = "\0";
+#define UART1_RX_BUFFER_SIZE 2
+#define MOVING_AVG_SIZE 3
+uint16_t adc_buffer[MOVING_AVG_SIZE] = {0};
+uint16_t adc_sum = 0;
+uint8_t buffer_index = 0;
+int samples_collected = 0;
+
+// Distance reading variables
+#define UART2_RX_BUFFER_SIZE 1
+uint8_t UART2_RX_Buffer[UART2_RX_BUFFER_SIZE];
+int flag = 0;
+float distance = 0;
+uint16_t count_1 = 0;
+uint16_t count_2 = 0;
+char string_1[40];
+const float min_distance = 10; // Minimum distance of 10cm
+volatile uint8_t object_detected = 0;           // 1 = recording triggered
+volatile uint8_t no_object_counter = 0;         // debounce counter
+const uint8_t no_object_timeout_ticks = 2;      // 2 ticks = 1000ms debounce
+
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_USART2_UART_Init(void);
+static void MX_USART1_UART_Init(void);
+static void MX_TIM1_Init(void);
+static void MX_TIM6_Init(void);
+static void MX_TIM16_Init(void);
+/* USER CODE BEGIN PFP */
+void Handle_UART_Command();
+// Distance Reading
+void delay_uS(uint16_t delay);
+void HCSR04_Read();
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_USART2_UART_Init();
+  MX_USART1_UART_Init();
+  MX_TIM1_Init();
+  MX_TIM6_Init();
+  MX_TIM16_Init();
+  /* USER CODE BEGIN 2 */
+  // Audio Reading
+  HAL_UART_Receive_IT(&huart1, RX_Buffer, UART1_RX_BUFFER_SIZE);
+  HAL_UART_Receive_IT(&huart2, UART2_RX_Buffer, UART2_RX_BUFFER_SIZE);
+
+  // Distance Reading
+  HAL_TIM_Base_Start (&htim6); // Timer to count to 10 microseconds
+  HAL_TIM_IC_Start_IT(&htim1, TIM_CHANNEL_1); // Timer to measure distance
+  HAL_TIM_Base_Start_IT(&htim16);
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  if (HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure LSE Drive Capability
+  */
+  HAL_PWR_EnableBkUpAccess();
+  __HAL_RCC_LSEDRIVE_CONFIG(RCC_LSEDRIVE_LOW);
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_LSE|RCC_OSCILLATORTYPE_MSI;
+  RCC_OscInitStruct.LSEState = RCC_LSE_ON;
+  RCC_OscInitStruct.MSIState = RCC_MSI_ON;
+  RCC_OscInitStruct.MSICalibrationValue = 0;
+  RCC_OscInitStruct.MSIClockRange = RCC_MSIRANGE_6;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_MSI;
+  RCC_OscInitStruct.PLL.PLLM = 1;
+  RCC_OscInitStruct.PLL.PLLN = 16;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV7;
+  RCC_OscInitStruct.PLL.PLLQ = RCC_PLLQ_DIV2;
+  RCC_OscInitStruct.PLL.PLLR = RCC_PLLR_DIV2;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Enable MSI Auto calibration
+  */
+  HAL_RCCEx_EnableMSIPLLMode();
+}
+
+/**
+  * @brief TIM1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM1_Init(void)
+{
+
+  /* USER CODE BEGIN TIM1_Init 0 */
+
+  /* USER CODE END TIM1_Init 0 */
+
+  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+  TIM_IC_InitTypeDef sConfigIC = {0};
+
+  /* USER CODE BEGIN TIM1_Init 1 */
+
+  /* USER CODE END TIM1_Init 1 */
+  htim1.Instance = TIM1;
+  htim1.Init.Prescaler = 32-1;
+  htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim1.Init.Period = 65535;
+  htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim1.Init.RepetitionCounter = 0;
+  htim1.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+  if (HAL_TIM_ConfigClockSource(&htim1, &sClockSourceConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_TIM_IC_Init(&htim1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterOutputTrigger2 = TIM_TRGO2_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim1, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sConfigIC.ICPolarity = TIM_INPUTCHANNELPOLARITY_BOTHEDGE;
+  sConfigIC.ICSelection = TIM_ICSELECTION_DIRECTTI;
+  sConfigIC.ICPrescaler = TIM_ICPSC_DIV1;
+  sConfigIC.ICFilter = 0;
+  if (HAL_TIM_IC_ConfigChannel(&htim1, &sConfigIC, TIM_CHANNEL_1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM1_Init 2 */
+
+  /* USER CODE END TIM1_Init 2 */
+
+}
+
+/**
+  * @brief TIM6 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM6_Init(void)
+{
+
+  /* USER CODE BEGIN TIM6_Init 0 */
+
+  /* USER CODE END TIM6_Init 0 */
+
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+
+  /* USER CODE BEGIN TIM6_Init 1 */
+
+  /* USER CODE END TIM6_Init 1 */
+  htim6.Instance = TIM6;
+  htim6.Init.Prescaler = 32-1;
+  htim6.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim6.Init.Period = 65535;
+  htim6.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim6) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim6, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM6_Init 2 */
+
+  /* USER CODE END TIM6_Init 2 */
+
+}
+
+/**
+  * @brief TIM16 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM16_Init(void)
+{
+
+  /* USER CODE BEGIN TIM16_Init 0 */
+
+  /* USER CODE END TIM16_Init 0 */
+
+  /* USER CODE BEGIN TIM16_Init 1 */
+
+  /* USER CODE END TIM16_Init 1 */
+  htim16.Instance = TIM16;
+  htim16.Init.Prescaler = 32000-1;
+  htim16.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim16.Init.Period = 500-1;
+  htim16.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim16.Init.RepetitionCounter = 0;
+  htim16.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim16) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM16_Init 2 */
+
+  /* USER CODE END TIM16_Init 2 */
+
+}
+
+/**
+  * @brief USART1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART1_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART1_Init 0 */
+
+  /* USER CODE END USART1_Init 0 */
+
+  /* USER CODE BEGIN USART1_Init 1 */
+
+  /* USER CODE END USART1_Init 1 */
+  huart1.Instance = USART1;
+  huart1.Init.BaudRate = 230400;
+  huart1.Init.WordLength = UART_WORDLENGTH_8B;
+  huart1.Init.StopBits = UART_STOPBITS_1;
+  huart1.Init.Parity = UART_PARITY_NONE;
+  huart1.Init.Mode = UART_MODE_TX_RX;
+  huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+  huart1.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+  huart1.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+  if (HAL_UART_Init(&huart1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART1_Init 2 */
+
+  /* USER CODE END USART1_Init 2 */
+
+}
+
+/**
+  * @brief USART2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART2_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART2_Init 0 */
+
+  /* USER CODE END USART2_Init 0 */
+
+  /* USER CODE BEGIN USART2_Init 1 */
+
+  /* USER CODE END USART2_Init 1 */
+  huart2.Instance = USART2;
+  huart2.Init.BaudRate = 230400;
+  huart2.Init.WordLength = UART_WORDLENGTH_8B;
+  huart2.Init.StopBits = UART_STOPBITS_1;
+  huart2.Init.Parity = UART_PARITY_NONE;
+  huart2.Init.Mode = UART_MODE_TX_RX;
+  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+  huart2.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+  huart2.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+  if (HAL_UART_Init(&huart2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART2_Init 2 */
+
+  /* USER CODE END USART2_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  /* USER CODE BEGIN MX_GPIO_Init_1 */
+
+  /* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, LD3_Pin|GPIO_PIN_4|GPIO_PIN_5, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : PA6 */
+  GPIO_InitStruct.Pin = GPIO_PIN_6;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : LD3_Pin PB4 PB5 */
+  GPIO_InitStruct.Pin = LD3_Pin|GPIO_PIN_4|GPIO_PIN_5;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /* USER CODE BEGIN MX_GPIO_Init_2 */
+
+  /* USER CODE END MX_GPIO_Init_2 */
+}
+
+/* USER CODE BEGIN 4 */
+// Pick Audio Recording Mode
+void Handle_UART_Command()
+{
+    if (UART2_RX_Buffer[0] == '1')
+    {
+        /* Enter manual recording mode: The Processing STM transmits the data as normal.
+         Go to HAL_UART_RxCpltCallback
+         Duration is determined by user in Python Script*/
+    	recording_mode = 1;
+		recording_active = 1;
+		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, GPIO_PIN_SET); // Blue LED ON for Manual Recording Mode
+		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_4, GPIO_PIN_RESET);
+		sprintf(string_1, "Manual Recording Mode Selected");
+		HAL_UART_Transmit(&huart2, (uint8_t*)string_1, strlen(string_1),10);
+    }
+    else if (UART2_RX_Buffer[0] == '2')
+    {
+        /* Enter Distance Trigger Mode: The Processing STM transmits the data as normal but
+         ends transmission when the distance is less than 10cm.*/
+    	recording_mode = 2;
+		recording_active = 1;
+		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_4, GPIO_PIN_SET); // Red LED ON for Distance Trigger Mode
+		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, GPIO_PIN_RESET);
+		sprintf(string_1, "Distance Trigger Mode Selected");
+		HAL_UART_Transmit(&huart2, (uint8_t*)string_1, strlen(string_1),10);
+    }
+    else
+    {
+        // Keep on reading ADC values through UART1. Go to HAL_UART_RxCpltCallback
+    	recording_mode = 1;
+		recording_active = 1;
+		sprintf(string_1, "Else Selected");
+		HAL_UART_Transmit(&huart2, (uint8_t*)string_1, strlen(string_1),10);
+		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, GPIO_PIN_RESET); // Turn OFF LEDs
+		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_4, GPIO_PIN_RESET);
+    }
+}
+/*****************Audio Reading Functions*****************/
+void HAL_UART_RxCpltCallback (UART_HandleTypeDef *huart)
+{
+	if (huart->Instance == USART1)
+	{
+		samples_collected += 1;
+		LSB_8 = RX_Buffer[0]; // Extract least significant 8 bits of the ADC value
+		MSB_8 = RX_Buffer[1]; // Extract most significant 8 bits of the ADC value
+
+//		// Construct 16-bit unsigned ADC value
+//		ADC_output = MSB_8;
+//		ADC_output = ADC_output << 8;
+//		ADC_output = ADC_output | LSB_8;
+
+		ADC_output = ((RX_Buffer[1] << 8) | RX_Buffer[0]);
+
+//		int8_t out_buf[2] = {0};
+//		out_buf[0] = ADC_output & 0xFF;
+//		out_buf[1] = (ADC_output >> 8) & 0xFF; // MSB
+
+		// MOVING AVERAGE FILTER
+		adc_sum -= adc_buffer[buffer_index];
+		adc_buffer[buffer_index] = ADC_output;
+		adc_sum += adc_buffer[buffer_index];
+		buffer_index = (buffer_index + 1) % MOVING_AVG_SIZE;
+		// Account for initial edge case where adc_buffer[1] to [3] is still 0
+		uint8_t tx_buf[2] = {0};
+		if (samples_collected >= 4)
+		{
+			uint16_t avg_adc_value = adc_sum/MOVING_AVG_SIZE;
+			tx_buf[0] = avg_adc_value & 0xFF;        // LSB
+			tx_buf[1] = (avg_adc_value >> 8) & 0xFF; // MSB
+			if (recording_mode == 1) // Manual Recording Mode
+			{
+				// Transmit avg_adc_value over UART under interrupt mode
+				HAL_UART_Transmit(&huart2, (uint8_t*)tx_buf,2,10);
+			}
+			else if (recording_mode == 2 && distance <= 10.0) // Distance Trigger Mode
+			{
+				if (recording_active == 1)
+				{
+					// Transmit avg_adc_value over UART under interrupt mode
+					HAL_UART_Transmit(&huart2, (uint8_t*)tx_buf,2,10);
+				}
+				else if (recording_active == 0)
+				{
+					// Stop transmission
+				}
+			}
+		}
+		HAL_UART_Receive_IT(&huart1, RX_Buffer, UART1_RX_BUFFER_SIZE); // Receive through UART1 Interrupt
+	}
+	else if (huart->Instance == USART2)
+	    {
+	        Handle_UART_Command();  // Call command handler
+	        HAL_UART_Receive_IT(&huart2, UART2_RX_Buffer, UART2_RX_BUFFER_SIZE); // Restart UART2 receive
+	    }
+}
+
+/*****************Distance Trigger Mode Functions*****************/
+void delay_uS(uint16_t delay)
+{
+	__HAL_TIM_SET_COUNTER(&htim6,0);
+	while(__HAL_TIM_GET_COUNTER(&htim6) < delay)
+	{
+
+	}
+}
+
+void HCSR04_Read()
+{
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, GPIO_PIN_SET);
+	delay_uS(10);
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, GPIO_PIN_RESET);
+}
+
+void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef *htim)
+{
+	if ((htim == &htim1) && (htim->Channel == 1))
+	{
+		if (flag == 0)
+		{
+			count_1 = HAL_TIM_ReadCapturedValue(&htim1, TIM_CHANNEL_1);
+			flag = 1;
+		}
+		else
+		{
+			count_2 = HAL_TIM_ReadCapturedValue(&htim1, TIM_CHANNEL_1);
+			__HAL_TIM_SET_COUNTER(htim,0);
+			if (count_2 >= count_1)
+			{
+			    distance = (count_2 - count_1) / 58.0;
+			}
+			else
+			{
+			    distance = ((0xFFFF - count_1) + count_2 + 1) / 58.0;
+			}
+
+			sprintf(string_1, "distance: %f cm\n", distance);
+			HAL_UART_Transmit(&huart2, (uint8_t*)string_1, strlen(string_1),10);
+			flag = 0;
+		}
+	}
+}
+
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+    if (htim == &htim16)
+    {
+    	if (recording_mode != 2)
+    	{
+    		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_3, GPIO_PIN_RESET);
+			return; // Only handle in distance mode
+    	}
+
+    	// Only do this if not interrupted
+    	HCSR04_Read();
+
+    	if (distance < min_distance)
+    	{
+    		no_object_counter = 0; // Reset debounce counter
+			if (!object_detected)
+			{
+				object_detected = 1;
+				recording_active = 1;
+
+				// Optional: feedback via LED or UART
+				HAL_GPIO_WritePin(GPIOB, GPIO_PIN_3, GPIO_PIN_SET); // LED ON
+				// HAL_UART_Transmit(&huart2, (uint8_t*)"Recording started\n", 18, 10);
+			}
+    	}
+    	else
+		{
+			if (object_detected)
+			{
+				no_object_counter++;
+				if (no_object_counter >= no_object_timeout_ticks)
+				{
+					object_detected = 0;
+					recording_active = 0;
+					HAL_GPIO_WritePin(GPIOB, GPIO_PIN_3, GPIO_PIN_RESET); // LED OFF
+					// HAL_UART_Transmit(&huart2, (uint8_t*)"Recording stopped\n", 18, 10);
+				}
+			}
+		}
+    }
+}
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
